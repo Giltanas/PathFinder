a7206912a466e3aa3cd727c4c2f548fcf57e9397
Nik Lytvynov <giltanas94@gmail.com>
2017-03-23 12:40:59 +0200

    Bad commit after crash pc fix

---
+++ PathFinder/HommFinder/Union.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HommFinder.Extensions.Finder.Extensions;
+
+namespace HommFinder
+{
+	public class Union
+	{
+		public Cell CellA { get; private set; }
+		public Cell CellB { get; private set; }
+		public double Weight { get; private set; }
+
+		public Union(Cell cellA, Cell cellB)
+		{
+			CellA = cellA;
+			CellB = cellB;
+
+			Weight = cellA.CellType.GetCellTypeWeight();
+		}
+	}
+}
