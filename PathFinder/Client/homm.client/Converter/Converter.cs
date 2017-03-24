66526a5225852064d4b12fad15f5522e84b6e5a9
Nik Lytvynov <giltanas94@gmail.com>
2017-03-24 16:03:01 +0200

    MapGameObject to cell converter add

---
+++ PathFinder/Client/homm.client/Converter/Converter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HoMM;
+using HoMM.ClientClasses;
+using HommFinder;
+
+namespace Homm.Client.Converter
+{
+	public static class Converter
+	{
+		public static IEnumerable<Direction> ConvertCellsToDirection(Stack<Cell> cells)
+		{
+			var direction = new List<Direction>();
+			var listCells = cells.ToList();
+			for (int i = 1; i < cells.Count; i++)
+			{
+				var x = listCells[i].X - listCells[i - 1].X;
+				var y = listCells[i].Y - listCells[i - 1].Y;
+				if (x == 1 && y == 1)
+				{
+					direction.Add(Direction.RightDown);
+					continue;
+				}
+				if (x == 0 && y == 1)
+				{
+					direction.Add(Direction.Down);
+					continue;
+				}
+				if (x == -1 && y == 1)
+				{
+					direction.Add(Direction.LeftDown);
+					continue;
+				}
+				if (x == 1 && y == -1)
+				{
+					direction.Add(Direction.RightUp);
+					continue;
+				}
+				if (x == 0 && y == -1)
+				{
+					direction.Add(Direction.Up);
+					continue;
+				}
+				if (x == -1 && y == -1)
+				{
+					direction.Add(Direction.LeftUp);
+				}
+			}
+			return direction;
+		}
+
+		public static Cell ConvertGMapObjectDataToCell(this MapObjectData mapObjectData)
+		{
+			var cellType = CellType.Road;
+
+			switch (mapObjectData.Terrain)
+			{
+				case Terrain.Road:
+					cellType = CellType.Road;
+					break;
+				case Terrain.Grass:
+					cellType = CellType.Grass;
+					break;
+				case Terrain.Snow:
+					cellType = CellType.Marsh;
+					break;
+				case Terrain.Marsh:
+					cellType = CellType.Marsh;
+					break;
+				case Terrain.Desert:
+					cellType = CellType.Snow;
+					break;
+			}
+			if (mapObjectData.Wall != null)
+			{
+				cellType = CellType.Block;
+			}
+			if (mapObjectData.NeutralArmy != null)
+			{
+				//TODO: Check army strength 
+				cellType = CellType.Block;
+			}
+			if (mapObjectData.Mine != null)
+			{
+				cellType = CellType.Block;
+			}
+
+			return new Cell(mapObjectData.Location.X, mapObjectData.Location.Y, cellType);
+		}
+	}
+}
--- PathFinder/Client/homm.client/Homm.Client.Program.cs
+++ PathFinder/Client/homm.client/Homm.Client.Program.cs
@@ -1,111 +1,130 @@
 ﻿using System;
 using System.Linq;
+using Homm.Client.Converter;
 using HoMM.Sensors;
 using HoMM;
 using HoMM.ClientClasses;
 using System.Collections.Generic;
+using HommFinder;
 
 namespace Homm.Client
 {
-    class Program
-    {
-        // Вставьте сюда свой личный CvarcTag для того, чтобы учавствовать в онлайн соревнованиях.
-        public static readonly Guid CvarcTag = Guid.Parse("00000000-0000-0000-0000-000000000000");
+	class Program
+	{
+		// Вставьте сюда свой личный CvarcTag для того, чтобы учавствовать в онлайн соревнованиях.
+		public static readonly Guid CvarcTag = Guid.Parse("00000000-0000-0000-0000-000000000000");
 
 
-        public static void Main(string[] args)
-        {
-            if (args.Length == 0)
-                args = new[] { "127.0.0.1", "18700" };
-            var ip = args[0];
-            var port = int.Parse(args[1]);
+		public static void Main(string[] args)
+		{
+			if (args.Length == 0)
+				args = new[] { "127.0.0.1", "18700" };
+			var ip = args[0];
+			var port = int.Parse(args[1]);
 
-            var client = new HommClient();
+			var client = new HommClient();
 
-            client.OnSensorDataReceived += Print;
-            client.OnInfo += OnInfo;
+			client.OnSensorDataReceived += Print;
+			client.OnInfo += OnInfo;
 
-            var sensorData = client.Configurate(
-                ip, port, CvarcTag,
+			var sensorData = client.Configurate(
+				ip, port, CvarcTag,
 
-                timeLimit: 90,              // Продолжительность матча в секундах (исключая время, которое "думает" ваша программа). 
+				timeLimit: 90,              // Продолжительность матча в секундах (исключая время, которое "думает" ваша программа). 
 
-                operationalTimeLimit: 20,   // Суммарное время в секундах, которое разрешается "думать" вашей программе. 
-                                            // Вы можете увеличить это время для отладки, чтобы ваш клиент не был отключен, 
-                                            // пока вы разглядываете программу в режиме дебаггинга.
+				operationalTimeLimit: 90,   // Суммарное время в секундах, которое разрешается "думать" вашей программе. 
+				// Вы можете увеличить это время для отладки, чтобы ваш клиент не был отключен, 
+				// пока вы разглядываете программу в режиме дебаггинга.
 
-                seed: 0,                    // Seed карты. Используйте этот параметр, чтобы получать одну и ту же карту и отлаживаться на ней.
-                                            // Иногда меняйте этот параметр, потому что ваш код должен хорошо работать на любой карте.
+				seed: 0,                    // Seed карты. Используйте этот параметр, чтобы получать одну и ту же карту и отлаживаться на ней.
+				// Иногда меняйте этот параметр, потому что ваш код должен хорошо работать на любой карте.
 
-                spectacularView: true,      // Вы можете отключить графон, заменив параметр на false.
+				spectacularView: true,      // Вы можете отключить графон, заменив параметр на false.
 
-                debugMap: false,            // Вы можете использовать отладочную простую карту, чтобы лучше понять, как устроен игоровой мир.
+				debugMap: false,            // Вы можете использовать отладочную простую карту, чтобы лучше понять, как устроен игоровой мир.
 
-                level: HommLevel.Level1,    // Здесь можно выбрать уровень. На уровне два на карте присутствует оппонент.
+				level: HommLevel.Level1,    // Здесь можно выбрать уровень. На уровне два на карте присутствует оппонент.
 
-                isOnLeftSide: true          // Вы можете указать, с какой стороны будет находиться замок героя при игре на втором уровне.
-                                            // Помните, что на сервере выбор стороны осуществляется случайным образом, поэтому ваш код
-                                            // должен работать одинаково хорошо в обоих случаях.
-            );
+				isOnLeftSide: true          // Вы можете указать, с какой стороны будет находиться замок героя при игре на втором уровне.
+				// Помните, что на сервере выбор стороны осуществляется случайным образом, поэтому ваш код
+				// должен работать одинаково хорошо в обоих случаях.
+			);
 
-            var path = new[] { Direction.RightDown, Direction.RightUp, Direction.RightDown, Direction.RightUp, Direction.LeftDown, Direction.Down, Direction.RightDown, Direction.RightDown, Direction.RightUp };
-            sensorData = client.HireUnits(1);
-            foreach (var e in path)
-                sensorData = client.Move(e);
-            sensorData = client.Move(Direction.RightDown);
-            client.Exit();
-        }
+			//var path = new[] { Direction.RightDown, Direction.RightUp, Direction.RightDown, Direction.RightUp, Direction.LeftDown, Direction.Down, Direction.RightDown, Direction.RightDown, Direction.RightUp };
+			//sensorData = client.HireUnits(1);
+			//foreach (var e in path)
+			//	sensorData = client.Move(e);
+			//sensorData = client.Move(Direction.RightDown);
+			var listCells = new List<Cell>();
+			foreach (var item in sensorData.Map.Objects)
+			{
+				listCells.Add(item.ConvertGMapObjectDataToCell());
+			}
 
-
-        static void Print(HommSensorData data)
-        {
-            Console.WriteLine("---------------------------------");
-
-            Console.WriteLine($"You are here: ({data.Location.X},{data.Location.Y})");
-
-            Console.WriteLine($"You have {data.MyTreasury.Select(z => z.Value + " " + z.Key).Aggregate((a, b) => a + ", " + b)}");
-
-            var location = data.Location.ToLocation();
-
-            Console.Write("W: ");
-            Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.Up)));
-
-            Console.Write("E: ");
-            Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.RightUp)));
-
-            Console.Write("D: ");
-            Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.RightDown)));
-
-            Console.Write("S: ");
-            Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.Down)));
-
-            Console.Write("A: ");
-            Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.LeftDown)));
-
-            Console.Write("Q: ");
-            Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.LeftUp)));
-        }
-
-        static string GetObjectAt(MapData map, Location location)
-        {
-            if (location.X < 0 || location.X >= map.Width || location.Y < 0 || location.Y >= map.Height)
-                return "Outside";
-            return map.Objects.
-                Where(x => x.Location.X == location.X && x.Location.Y == location.Y)
-                .FirstOrDefault()?.ToString() ?? "Nothing";
-        }
+			var pathFinder = new Finder(listCells);
+			var a = pathFinder.GetMoves(sensorData.Map.Objects
+				.Single(o => o.Location.X == sensorData.Location.X && o.Location.Y == sensorData.Location.Y)
+					.ConvertGMapObjectDataToCell(), sensorData.Map.Objects
+				.Single(o => o.Location.X == 9 && o.Location.Y == 2)
+					.ConvertGMapObjectDataToCell());
+			var b = Converter.Converter.ConvertCellsToDirection(a);
+			foreach (var item in b)
+			{
+				client.Move(item);
+			}
+			client.Exit();
+		}
 
 
-        static void OnInfo(string infoMessage)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(infoMessage);
-            Console.ResetColor();
-        }
+		static void Print(HommSensorData data)
+		{
+			Console.WriteLine("---------------------------------");
+
+			//Console.WriteLine($"You are here: ({data.Location.X},{data.Location.Y})");
+
+			//  Console.WriteLine($"You have {data.MyTreasury.Select(z => z.Value + " " + z.Key).Aggregate((a, b) => a + ", " + b)}");
+
+			//var location = data.Location.ToLocation();
+
+			//Console.Write("W: ");
+			//Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.Up)));
+
+			//Console.Write("E: ");
+			//Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.RightUp)));
+
+			//Console.Write("D: ");
+			//Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.RightDown)));
+
+			//Console.Write("S: ");
+			//Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.Down)));
+
+			//Console.Write("A: ");
+			//Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.LeftDown)));
+
+			//Console.Write("Q: ");
+			//Console.WriteLine(GetObjectAt(data.Map, location.NeighborAt(Direction.LeftUp)));
+		}
+
+		//static string GetObjectAt(MapData map, Location location)
+		//{
+		//	if (location.X < 0 || location.X >= map.Width || location.Y < 0 || location.Y >= map.Height)
+		//		return "Outside";
+		//	return map.Objects.
+		//		Where(x => x.Location.X == location.X && x.Location.Y == location.Y)
+		//		.FirstOrDefault()?.ToString() ?? "Nothing";
+		//}
+
+
+		static void OnInfo(string infoMessage)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(infoMessage);
+			Console.ResetColor();
+		}
 
 
 
 
-       
-    }
+
+	}
 }
