457efa0aa9202069ceda4e2b8b12c69e55dd5ed8
tetiana.zhydeikina.kn.2016@lpnu.ua <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-03-28 14:25:18 +0300

    fix errors in usings

--- PathFinder/HommFinder/Union.cs
+++ PathFinder/HommFinder/Union.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HommFinder.Extensions.Finder.Extensions;
+using HommFinder.Extensions;
 
 namespace HommFinder
 {
